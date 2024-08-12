58127444fc4b744bebcea874bcc95b60fba3f5f3
FerHerrera17 <mejiaenrique154@gmail.com>
2024-08-12 17:17:28 -0600

    Detalles

--- src/examen1_parcial1_fernandoherrera/Muelle.java
+++ src/examen1_parcial1_fernandoherrera/Muelle.java
@@ -26,6 +26,7 @@ public class Muelle {
         System.out.println("2. Agregar Elemento");
         System.out.println("3. Vaciar Barco");
         System.out.println("4. Lista de Barcos");
+        System.out.println("5. Salir");
         
         int op = read.nextInt();
         
@@ -36,15 +37,43 @@ public class Muelle {
                 agregarBarco(nom_barco);
                 break;
             case 2:
+                System.out.println("Ingrese el nombre del barco: ");
+                String nombre = read.nextLine();
+                agregarElemento(nombre);
                 break;
             case 3:
+                System.out.println("Ingrese el nombre del barco: ");
+                nombre = read.nextLine();
+                double total = vaciarBarco(nombre);
+                System.out.println("Total generado: Lps. " + total);
                 break;
             case 4:
+                System.out.println("Ingrese el año de referencia: ");
+                int año = read.nextInt();
+                barcoFecha(año);
                 break;
+            case 5:
+                System.out.println("Nos vemos....");
             default:
+                System.out.println("La opcion que ingreso no es valida");
                 break;
-                
-                
+        }
+        
+        public void agregarBarco (String nom_barco){
+            Scanner read = new Scanner (System.in);
+            
+            System.out.println("Ingrese nuevamente el nombre del barco: ");
+            String nombre = read.nextLine();
+            
+            for (Barco barco : barcos) {
+                if (barco.getNombre().equals(nombre)){
+                    System.out.println("Ya existe un barco con ese nombre");
+                    return;
+                }
+                else if (tipo.equals("PESQUERO")){
+                    
+                }
+            }
         }
     }
 
